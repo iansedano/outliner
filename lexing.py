c3bcff740c68602c9e6e5e24781537b1a8d1511f
ian <iansedano@gmail.com>
2021-01-01 19:54:17 +0100

    implemented token class into lexer

--- lexing.py
+++ lexing.py
@@ -17,31 +17,37 @@ def lexer(path):
 	with open(path) as mydata:
 			data = mydata.read()
 
-	lexemes = []
 	current_lexeme = []
 	current_line = 1;
 
 	for i, c in enumerate(data):
 
 		if i == 0 and c in STRUCTURE_TOKENS:
-			current_tok = token(STRUCTURE_TOKENS[c], c, current_line)
-			lexemes.append(STRUCTURE_TOKENS[c])
+			current_tok = token(STRUCTURE_TOKENS[c], repr(c), current_line)
+			root = current_tok # If the first character is not a STRUCTURE_TOKEN this program will break
 		elif c in STRUCTURE_TOKENS:
 			if current_lexeme != []:
-				lexemes.append(''.join(current_lexeme).strip())
+				lexeme_tok = token('<STRING>', ''.join(current_lexeme).strip(), current_line)
+				previous_tok.next = lexeme_tok
+				previous_tok = lexeme_tok
 				current_lexeme = []
-			lexemes.append(STRUCTURE_TOKENS[c])
+			current_tok = token(STRUCTURE_TOKENS[c], repr(c), current_line)
+			previous_tok.next = current_tok
 		else:
 			current_lexeme.append(c)
 
+		previous_tok = current_tok
 		current_line = current_line + 1 if c == '\n' else current_line
 		
-	return lexemes
+	return root
+
+def print_lex(tok):
+
+	while tok:
+		print(tok.tok_type, tok.value, tok.line_no)
+		tok = tok.next
 
 if __name__ == "__main__":
-	lexemes = lexer('test_files\\folderstruct1.txt')
-	print(lexemes)
+
 	lexemes = lexer('test_files\\folderstruct2.txt')
-	print(lexemes)
-	lexemes = lexer('test_files\\folderstruct3.txt')
-	print(lexemes)
+	print_lex(lexemes)
