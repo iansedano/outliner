a8d987c0094a7e5b493d60946d24b6cc7afe60e3
ian <iansedano@gmail.com>
2021-01-01 22:48:44 +0100

    refactor and clean up of lexer

--- lexing.py
+++ lexing.py
@@ -22,25 +22,48 @@ def lexer(path):
 
 	for i, c in enumerate(data):
 
-		if i == 0 and c in STRUCTURE_TOKENS:
-			current_tok = token(STRUCTURE_TOKENS[c], repr(c), current_line)
-			root = current_tok # If the first character is not a STRUCTURE_TOKEN this program will break
-		elif c in STRUCTURE_TOKENS:
+		if c in STRUCTURE_TOKENS:
 			if current_lexeme != []:
-				lexeme_tok = token('<STRING>', ''.join(current_lexeme).strip(), current_line)
+
+				# Create trailing lexeme token
+				lexeme_tok = token(
+					'<STRING>',
+					''.join(current_lexeme).strip(),
+					current_line
+					)
+
 				previous_tok.next = lexeme_tok
 				previous_tok = lexeme_tok
 				current_lexeme = []
-			current_tok = token(STRUCTURE_TOKENS[c], repr(c), current_line)
-			previous_tok.next = current_tok
+
+			# Create structure token
+			current_tok = token(
+				STRUCTURE_TOKENS[c],
+				repr(c),
+				current_line
+				)
+
+			# Set root
+			if current_line == 1:
+				root = current_tok
+			else:
+				previous_tok.next = current_tok
+		
 		else:
 			current_lexeme.append(c)
 
 		previous_tok = current_tok
 		current_line = current_line + 1 if c == '\n' else current_line
 
+	# Deal with the last line since no EOF detection
 	if current_lexeme != []:
-		lexeme_tok = token('<STRING>', ''.join(current_lexeme).strip(), current_line)
+
+		lexeme_tok = token(
+			'<STRING>',
+			''.join(current_lexeme).strip(),
+			current_line
+			)
+
 		previous_tok.next = lexeme_tok
 		
 	return root
@@ -53,6 +76,12 @@ def print_lex(tok):
 
 
 if __name__ == "__main__":
-
+	print("======================================")
+	lexemes = lexer('test_files\\folderstruct1.txt')
+	print_lex(lexemes)
+	print("======================================")
 	lexemes = lexer('test_files\\folderstruct2.txt')
+	print_lex(lexemes)
+	print("======================================")
+	lexemes = lexer('test_files\\folderstruct3.txt')
 	print_lex(lexemes)
